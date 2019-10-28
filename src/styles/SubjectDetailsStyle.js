9104a419998532d5fd0f8e7529a90f87be4489c1
Marija <marija.jovanoska.maca@gmail.com>
2019-10-28 13:08:58 +0100

    ADD style for SubjectDetails

---
+++ src/styles/SubjectDetailsStyle.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+`;
+
+export const Title = styled.div`
+margin: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const Info = styled.div`
+  margin: 20px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const Description = styled.div`
+  margin: 20px 30px;
+  text-align: justify;
+  text-indent: 40px;
+`;
