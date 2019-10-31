c1c6c7c520c622b169c9b78dcfdcfaacc90bd841
Marija <marija.jovanoska.maca@gmail.com>
2019-10-31 13:51:23 +0100

    UPD make manu responsive

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "@reach/router";
 import { Header, NavigationBar } from "../styles/MenuStyle";
+import { MenuIcon } from "../icons/Icons";
 
 const NavLink = props => (
   <Link
     {...props}
     getProps={({ isCurrent }) => {
-      // the object returned here is passed to the
-      // anchor element's props
       return {
         style: {
+          color: isCurrent ? "#97BCC7" :  "#006884" ,
           fontStyle: isCurrent ? "italic" : null,
           textShadow: isCurrent ? "2px 2px 5px rgba(128,128,128,0.4)" : null
         }
@@ -18,18 +18,46 @@ const NavLink = props => (
   />
 );
 
-const Menu = () => {
-  return (
-    <Header>
-      <NavigationBar>
-        <NavLink to="/">University</NavLink>
-      </NavigationBar>
-      <NavigationBar>
-        <NavLink to="/subjects">Subjects</NavLink>
-        <NavLink to="/selected">Selected</NavLink>
-      </NavigationBar>
-    </Header>
-  );
-};
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false
+    };
+  }
+  handleToggle = e => {
+    e.preventDefault();
+    this.setState(
+      {
+        isExpanded: !this.state.isExpanded
+      },
+      console.log("Posle", this.state.isExpanded)
+    );
+  };
+
+  render() {
+    const { isExpanded } = this.state;
+    return (
+      <Header>
+        <NavigationBar>
+          <NavLink to="/">University</NavLink>
+        </NavigationBar>
+        <div
+          className="menu-icon"
+          aria-hidden="true"
+          onClick={e => this.handleToggle(e)}
+        >
+          <MenuIcon />
+        </div>
+        <NavigationBar>
+          <ul className={`collapsed ${isExpanded ? "is-expanded" : ""}`}>
+            <NavLink to="/subjects">Subjects</NavLink>
+            <NavLink to="/selected">Selected</NavLink>
+          </ul>
+        </NavigationBar>
+      </Header>
+    );
+  }
+}
 
 export default Menu;
--- src/styles/MenuStyle.js
+++ src/styles/MenuStyle.js
@@ -3,28 +3,76 @@ import styled from "styled-components";
 export const Header = styled.div`
   box-sizing: border-box;
   display: flex;
+  align-items: center;
+  padding-left: 10px;
+  z-index: 1;
   justify-content: space-between;
   border: none;
-  padding: 10px 0 10px 40px;
   border-radius: 0 0 6px 6px;
-  color: #053D57;
+  color: #053d57;
   background-color: white;
   box-shadow: 1px 1px 4px 1px rgba(177, 177, 177, 0.4);
+
+  .menu-icon {
+    display: none;
+  }
+
+  @media only screen and (max-width: 600px) {
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    min-height: 56px;
+    position: relative;
+    padding: 15px;
+
+    .menu-icon {
+      display: inline-block;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      cursor: pointer;
+    }
+
+    ul.collapsed {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+      flex-wrap: wrap;
+      overflow: hidden;
+      max-height: 0;
+      padding: 0;
+      margin: 0;
+
+      &.is-expanded {
+        overflow: hidden;
+        max-height: 500px;
+        padding-top: 10px;
+        /* margin: 10px; */
+        /* padding: 20px; */
+        /* background-color: red; */
+      }
+    }
+  }
 `;
 
 export const NavigationBar = styled.div`
-  display: flex;
-  justify-content: flex-end;
   a {
     text-decoration: none;
     margin-right: 40px;
-    color: #053D57;
+    color: #053d57;
     font-size: 18px;
     font-weight: bold;
     text-transform: uppercase;
     letter-spacing: 1px;
-    :hover{
-      color: #97BCC7
+    padding: 0;
+    :hover {
+      color: #97bcc7;
     }
+  }
+  @media only screen and (max-width: 600px) {
+    padding: 0;
+    margin: 0;
   }
 `;
