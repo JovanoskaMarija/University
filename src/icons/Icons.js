b4cac47a0e2a0fa3c497d73b6cff6fa9c7420821
Marija <marija.jovanoska.maca@gmail.com>
2019-10-31 13:47:30 +0100

    UPD create new menu icon

--- src/icons/Icons.js
+++ src/icons/Icons.js
@@ -7,6 +7,7 @@ import {u1F354} from 'react-icons-kit/noto_emoji_regular/u1F354'
 import {u2615} from 'react-icons-kit/noto_emoji_regular/u2615'
 import {u1F3B8} from 'react-icons-kit/noto_emoji_regular/u1F3B8'
 import {menu4} from 'react-icons-kit/icomoon/menu4'
+import {ic_menu} from 'react-icons-kit/md/ic_menu'
 
 const IconContainer = 
 withBaseIcon({ size: "100%", style:{width: 40, height: 40, color: "rgba(5,61,87,0.8)"} })
@@ -18,3 +19,4 @@ export const IconHamburger = () => <IconContainer icon={u1F354} />
 export const IconCafe = () => <IconContainer icon={u2615} />
 export const IconGuitar = () => <IconContainer icon={u1F3B8} />
 export const IconMenu = () => <IconContainer icon={menu4} />
+export const MenuIcon = () => <IconContainer icon={ic_menu} />
