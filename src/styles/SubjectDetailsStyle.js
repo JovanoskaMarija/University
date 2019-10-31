cdcbc5f9cca3484d40935f5a9609d73246ad58ba
Marija <marija.jovanoska.maca@gmail.com>
2019-10-31 14:25:30 +0100

    UPD make details page responsive

--- src/styles/SubjectDetailsStyle.js
+++ src/styles/SubjectDetailsStyle.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const Details = styled.div`
+ box-sizing: border-box;
   display: flex;
   flex-direction: column;
 `;
 
 export const Header = styled.div`
+ box-sizing: border-box;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -13,6 +15,7 @@ export const Header = styled.div`
 `;
 
 export const Title = styled.div`
+ box-sizing: border-box;
   margin: 0 20px;
   display: flex;
   justify-content: center;
@@ -26,17 +29,34 @@ export const Title = styled.div`
     font-size: 30px;
     font-weight: normal;
   }
+
+  @media only screen and (max-width: 600px) {
+    margin-left: 0;
+    margin-right: 0;
+  }
 `;
 
 export const Info = styled.div`
+ box-sizing: border-box;
   margin: 20px 0 40px 0;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  @media only screen and (max-width: 600px) {
+    width: 95%;
+    margin-left: 0;
+    margin-right: 0;
+    display: flex;
+    justify-content: space-around;
+    padding-left: 10px;
+  }
 `;
 
 export const Description = styled.div`
-  height: calc(100vh - 342.6px);
+  box-sizing: border-box;
+  height: auto;
+  margin: 0 20px;
+  box-shadow: 1px 1px 4px 1px rgba(177, 177, 177, 0.4);
   padding: 20px 30px;
   font-size: 20px;
   letter-spacing: 1px;
@@ -45,4 +65,9 @@ export const Description = styled.div`
   text-indent: 40px;
   color: #053d57;
   background-color: #f2f1ef;
+
+  @media only screen and (max-width: 600px) {
+    margin-left: 0;
+    margin-right: 0;
+  }
 `;
