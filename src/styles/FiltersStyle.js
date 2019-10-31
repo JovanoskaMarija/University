f09c3acf37908d527c84009c4bfcee57fd661dda
Marija <marija.jovanoska.maca@gmail.com>
2019-10-31 14:08:19 +0100

    UPD make filters responesive

--- src/styles/FiltersStyle.js
+++ src/styles/FiltersStyle.js
@@ -4,17 +4,29 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row-reverse;
   justify-content: space-between;
+
+  @media only screen and (max-width: 1050px){
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center
+  }
 `;
 
 export const Filter = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: flex-start;
+  justify-content: center;
   align-content: center;
   align-items: center;
   flex-wrap: wrap;
   box-sizing: border-box;
   padding: 10px;
+  @media only screen and (max-width: 1050px){
+    display: flex;
+    align-items: center;
+    justify-content: center
+  }
  
 `;
 
@@ -23,7 +35,6 @@ export const Separator = styled.div`
 `;
 
 export const ToggleFilters = styled.div`
-  
   padding: 10px ;
-  margin: 10px
+  margin: 10px;
 `;
